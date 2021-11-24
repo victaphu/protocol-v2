59bc0c565aeb347297a97bd5381ece9c244df6df
victaphu <victaphu@gmail.com>
2021-11-24 22:30:27 +0800

    update: added chainlink aggregator to constants

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -84,6 +84,8 @@ export const chainlinkAggregatorProxy = {
   mumbai: '0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada',
   avalanche: '0x0A77230d17318075983913bC2145DB16C7366156',
   fuji: '0x5498BB86BC934c8D34FDA08E81D444153d0D06aD',
+  harmonyTest: '0x7466d7d0C21Fa05F32F5a0Fa27e12bdC06348Ce2',
+  harmony: '',
   tenderly: '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419',
 };
 
@@ -94,5 +96,7 @@ export const chainlinkEthUsdAggregatorProxy = {
   mumbai: '0x0715A7794a1dc8e42615F059dD6e406A6594651A',
   avalanche: '0x976B3D034E162d8bD72D6b9C989d545b839003b0',
   fuji: '0x86d67c3D38D2bCeE722E601025C25a575021c6EA',
+  harmonyTest: '0x1E120B3b4aF96e7F394ECAF84375b1C661830013',
+  harmony: '',
   tenderly: '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419',
 };
